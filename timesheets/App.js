4adca4e508ebb4f53bd41fd4e7e0ed9ec1698c0f
phin2 <nguyephi24@up.edu>
2022-02-02 16:18:45 -0800

    Added icons to Navbar

--- timesheets/App.js
+++ timesheets/App.js
@@ -2,37 +2,62 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
+//The Home Screen
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Home Page</Text>
     </View>
   );
 }
 
+//The Setting Page
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Settings Page</Text>
     </View>
   );
 }
 
+//Timesheets page
 function TimesheetScreen() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Timesheets!</Text>
+      <Text>Timesheets Page</Text>
     </View>
   )
 }
 
+//creates the navigation bar
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    //adds the different tabs to the navigation bar 
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-build' : 'ios-build-outline';
+            } else if (route.name === 'Timesheet') {
+              iconName = focused ? 'ios-document-text':'ios-document-text-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Timesheets" component={TimesheetScreen} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
