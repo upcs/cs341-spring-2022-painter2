2845d80baf942de7e6ea53ad9f4a95cda028e6de
phin2 <nguyephi24@up.edu>
2022-02-15 01:19:03 -0800

    Merged Kamali Branch

--- timesheets/navigation/Screens/homescreen.js
+++ timesheets/navigation/Screens/homescreen.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import MainContainer from './navigation/MainContainer';
 import { useState, useEffect } from 'react';
 import { Text, View, TextInput, StyleSheet, Button, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
