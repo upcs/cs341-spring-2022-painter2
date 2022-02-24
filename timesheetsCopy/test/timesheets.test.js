81f4d71de0f727f3d7f9827840eddd190741b4bc
rajan23-git <83401124+rajan23-git@users.noreply.github.com>
2022-02-23 22:16:01 -0800

    Delete timesheetsCopy directory

--- timesheetsCopy/test/timesheets.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import TimesheetScreen from '../navigation/Screens/timesheets'
-
-test('Timesheets screen renders correctly', () => {
-    const timesheets = renderer.create(<TimesheetScreen/>).toJSON;
-    expect(timesheets).toMatchSnapshot
-})
