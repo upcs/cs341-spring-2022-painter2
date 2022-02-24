c7a3e7f9d7f6c3c3fd2c3a91cc74b64093870c39
phin2 <nguyephi24@up.edu>
2022-02-23 17:55:22 -0800

    Update timesheets.js

--- timesheets/navigation/Screens/timesheets.js
+++ timesheets/navigation/Screens/timesheets.js
@@ -6,6 +6,36 @@ import styles from './styles/timesheetStyle.js';
 export default function TimesheetScreen() {
     const data = require("./data.json"); // in the future data wull be pulled from the database
 
+<<<<<<< Updated upstream
+=======
+const firebaseConfig = {
+  apiKey: "AIzaSyCVu8npmz8_Mes5xQC6LBYTEBaw55ucAxRJXc",
+  authDomain: "timesheetdb-2b167.firebaseapp.com",
+  projectId: "timesheetdb-2b167",
+  storageBucket: "timesheetdb-2b167.appspot.com",
+  messagingSenderId: "533714654432",
+  appId: "1:533714654432:web:9a8adf4fa6f391b48f6c85",
+  measurementId: "G-S9ZRZDN57B"
+};
+
+if(firebase.apps.length==0){
+  firebase.initializeApp(firebaseConfig);
+ }
+
+
+export default function TimesheetScreen({ navigation }) {
+  const [tsData, setTSData] = useState([]);
+  const data = require("./data.json");  // in the future data wull be pulled from the database
+
+ const getTimesheets = async () => {
+    const snapshot = await firebase.firestore().collection('clocking').get()
+    const timesheetsData = []
+    snapshot.forEach(doc => {
+        timesheetsData.push(doc.data());
+    });
+    setTSData(timesheetsData);
+}     
+>>>>>>> Stashed changes
       const Item = ({ name }) => (
         <View style={styles.body}>
           <Text styles={styles.bodyText}>{name}</Text>
