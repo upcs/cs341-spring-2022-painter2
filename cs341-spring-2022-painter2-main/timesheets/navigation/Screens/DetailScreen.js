2c1e85f9d94e899d69cf2798756396ec3e87b59c
phin2 <nguyephi24@up.edu>
2022-03-15 22:05:48 -0700

    Added more account heirarchy

--- cs341-spring-2022-painter2-main/timesheets/navigation/Screens/DetailScreen.js
+++ cs341-spring-2022-painter2-main/timesheets/navigation/Screens/DetailScreen.js
@@ -9,44 +9,84 @@ import 'firebase/firestore';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { useContext } from 'react';
+import AppContext from '../Context.js';
 
 export default function DetailScreen({ route, navigation }) {
     
     const [edit, setEdit] = useState(false);
     const {name, date, clockIn, clockOut, id } = route.params;
-    return (
-        <View>                
-            <View style={styles.header}>
-                <Text style={styles.headerText}>
-                    {date}{"\n"}{name}
-                </Text>
-            </View> 
-            { edit ? 
-            <View>
-                <Text>Time In:</Text>
-                    <TextInput style={styles2.input} 
-                    placeholder = {clockIn} 
-                    editable ={true} 
-                    onSubmitEditing = {newTimeIn => alert(newTimeIn.nativeEvent.text)}/> 
-                <Text>Time Out:</Text>
-                    <TextInput style={styles2.input} 
-                    placeholder = {clockOut} 
-                    editable ={true} 
-                    onSubmitEditing ={newTimeOut => alert(id,newTimeOut.nativeEvent.text)}/> 
-            </View>:
-            <View>
-                <Text>Time In:</Text>
-                    <TextInput style={styles2.input} 
-                    value = {clockIn} 
-                    editable ={false}/> 
-                <Text>Time Out:</Text>
-                    <TextInput style={styles2.input} 
-                    value = {clockOut} 
-                    editable ={false}/> 
-            </View>}
-            <Button title= "Edit" style={{height:65,marginTop:15,position:"absolute"}}onPress={() => setEdit(!edit)}/>
-        </View>
-    )
+    const tsContext = useContext(AppContext);
+
+    if(tsContext.currentRole == 'Employee') {
+        return (
+            <View>                
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>
+                        {date}{"\n"}{name}
+                    </Text>
+                </View> 
+                { edit ? 
+                <View>
+                    <Text>Time In:</Text>
+                        <TextInput style={styles2.input} 
+                        placeholder = {clockIn} 
+                        editable ={true} 
+                        onSubmitEditing = {newTimeIn => alert(newTimeIn.nativeEvent.text)}/> 
+                    <Text>Time Out:</Text>
+                        <TextInput style={styles2.input} 
+                        placeholder = {clockOut} 
+                        editable ={true} 
+                        onSubmitEditing ={newTimeOut => alert(id,newTimeOut.nativeEvent.text)}/> 
+                </View>:
+                <View>
+                    <Text>Time In:</Text>
+                        <TextInput style={styles2.input} 
+                        value = {clockIn} 
+                        editable ={false}/> 
+                    <Text>Time Out:</Text>
+                        <TextInput style={styles2.input} 
+                        value = {clockOut} 
+                        editable ={false}/> 
+                </View>}
+            </View>
+        )
+    } else {
+        return (
+            <View>                
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>
+                        {date}{"\n"}{name}
+                    </Text>
+                </View> 
+                { edit ? 
+                <View>
+                    <Text>Time In:</Text>
+                        <TextInput style={styles2.input} 
+                        placeholder = {clockIn} 
+                        editable ={true} 
+                        onSubmitEditing = {newTimeIn => alert(newTimeIn.nativeEvent.text)}/> 
+                    <Text>Time Out:</Text>
+                        <TextInput style={styles2.input} 
+                        placeholder = {clockOut} 
+                        editable ={true} 
+                        onSubmitEditing ={newTimeOut => alert(id,newTimeOut.nativeEvent.text)}/> 
+                </View>:
+                <View>
+                    <Text>Time In:</Text>
+                        <TextInput style={styles2.input} 
+                        value = {clockIn} 
+                        editable ={false}/> 
+                    <Text>Time Out:</Text>
+                        <TextInput style={styles2.input} 
+                        value = {clockOut} 
+                        editable ={false}/> 
+                </View>}
+                <Button title= "Edit" style={{height:65,marginTop:15,position:"absolute"}}onPress={() => setEdit(!edit)}/>
+            </View>
+        )
+    }
+   
 }
 
 const styles2 = StyleSheet.create({
