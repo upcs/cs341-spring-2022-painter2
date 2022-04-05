105c38e367a031fee942e30154cf24407c655701
phin2 <nguyephi24@up.edu>
2022-04-05 15:25:08 -0700

    ClockIn and Clockout have 2 sets of coordinates

--- cs341-spring-2022-painter2-main/timesheets/navigation/Screens/databaseFunctions.js
+++ cs341-spring-2022-painter2-main/timesheets/navigation/Screens/databaseFunctions.js
@@ -254,7 +254,7 @@ export const editEmployeeEmailHelper = async (docIDInput,emailInput)=>{
 //with the employee name, employee ID, date and 
 //clock in time as the parameter
  export const clockInFunc= async(newName,newEmployeeID,newDate
-,newClockInTime, newJobSite, newTask, newLatitude, newLongitude )=>{
+,newClockInTime, newJobSite, newTask, ciLatitude, ciLongitude)=>{
     //gets all clock records for a given employee
     var clockRecords= await firebase.firestore()
     .collection('clocking')
@@ -296,8 +296,10 @@ export const editEmployeeEmailHelper = async (docIDInput,emailInput)=>{
         jobSite:newJobSite, 
         hoursWorked:0, 
         task:newTask,
-        latitude:newLatitude,
-        longitude:newLongitude
+        ClockInLatitude:ciLatitude,
+        ClockInLongitude:ciLongitude,
+        ClockOutLatitude: null,
+        ClockOutLongitude: null
         });
       
     
@@ -309,14 +311,19 @@ export const editEmployeeEmailHelper = async (docIDInput,emailInput)=>{
         firebase.firestore()
         .collection("clocking")
         .add({
-        name:newName,
-        employeeID:newEmployeeID,
-        date:newDate,
-        clockID:0,
-        clockIn:newClockInTime,
-        clockOut:null,
-        hoursWorked:0  
-   
+          name:newName,
+          employeeID:newEmployeeID,
+          date:newDate,
+          clockID:0,
+          clockIn:newClockInTime,
+          clockOut:null,
+          jobSite:newJobSite, 
+          hoursWorked:0, 
+          task:newTask,
+          ClockInLatitude:ciLatitude,
+          ClockInLongitude:ciLongitude,
+          ClockOutLatitude: null,
+          ClockOutLongitude: null
         });
 
 
@@ -372,8 +379,8 @@ if(clockOutField===null){
     .update({
         clockOut:newClockOutTime,
         hoursWorked:newHoursWorked,
-        latitude:newLatitude,
-        longitude:newLongitude
+        ClockOutLatitude:newLatitude,
+        ClockOutLongitude:newLongitude
     })
      .then(() => {
     console.log('clock record updated!');
